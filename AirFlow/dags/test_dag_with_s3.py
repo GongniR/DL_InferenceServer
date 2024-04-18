9698a1d4cd033d16321d60ba8a6eb551a17110be
GongniR <pakhomovd0@gmail.com>
2024-04-18 17:05:18 +0300

    update

--- AirFlow/dags/test_dag_with_s3.py
+++ AirFlow/dags/test_dag_with_s3.py
@@ -27,16 +27,9 @@ def my_very_own_etl():
 
     @task()
     def load(data: List[str]):
-        hook = S3Hook(aws_conn_id="S3_ETL_CONN")
-        extra = hook.get_connection(hook.aws_conn_id).get_extra()
-        bucket_name = json.loads(extra).get('bucket_name')
-        if bucket_name:
-            for url in data:
-                r = requests.get(url)
-                r.raise_for_status()
-                hook.load_bytes(r.content, key=url.split('/')[-1], bucket_name=bucket_name)
-        else:
-            raise ValueError("Bucket name not found in connection extra")
+        for url in data:
+            r = requests.get(url)
+            r.raise_for_status()
 
     extracted_data = extract()
     transformed_data = transform(extracted_data)
